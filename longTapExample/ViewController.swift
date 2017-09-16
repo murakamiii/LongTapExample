7dd60f4ab4fff7f9372ebf78231bfa572f1790b6
murakamiii <9khc727ru7@gmail.com>
2017-09-16 10:38:25 +0900

    Initial Commit

---
+++ longTapExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  longTapExample
+//
+//  Created by MURAKAMI on 2017/09/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
